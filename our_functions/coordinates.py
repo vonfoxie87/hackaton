8a9cf68b1862ea1d5e3dd84877c725024bae757a
Arjan Démoed <ajdemoed@hotmail.com>
2022-02-23 11:57:54 +0100

    add function get_coordinates

--- our_functions/coordinates.py
+++ our_functions/coordinates.py
@@ -1,6 +1,13 @@
 import pandas as pd
-import sqlite3
-
 
 def get_coordinates():
-    df = pd.read_csv('data/coordinaten.csv')
+    data = pd.read_csv('../data/coordinaten.csv')
+    # maak dataframe
+    df = pd.DataFrame(data) 
+    # maak losse rijen
+    df_records = df.to_records(index=False)
+    # maak de list
+    df_list = list(df_records)
+
+    return df_list
+
