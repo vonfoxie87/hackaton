096b27e5c0276202314f487a1a9abd5aa6d0d5a1
Vos Nils (N.H.) <nils.vos@politie.nl>
2022-02-23 11:59:25 +0100

    nieuw

--- app.py
+++ app.py
@@ -3,19 +3,32 @@ from our_functions.coordinates import get_coordinates
 import folium
 
 
-
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    test = get_coordinates()
-    print(test)
+    coordinaten = get_coordinates()
 
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    start_coords = (51.9066667, 4.188611111111111)
+    folium_map = folium.Map(location=start_coords, zoom_start=17)
+
+
+    folium.Marker(
+        location=(51.9066667, 4.188611111111111),
+        popup="Start",
+        icon=folium.Icon(color="green"),
+    ).add_to(folium_map)
+    folium_map
+
+    folium.PolyLine(
+        locations=[(51.9066667,4.188611111111111),(51.9044444,4.184166666666667)],
+        color="red",
+        weight=2.5,
+        opacity=1
+    ).add_to(folium_map)
+    folium_map
+
     return folium_map._repr_html_()
-
 
 
 
--- our_functions/coordinates.py
+++ our_functions/coordinates.py
@@ -4,3 +4,4 @@ import sqlite3
 
 def get_coordinates():
     df = pd.read_csv('data/coordinaten.csv')
+    print(df)
